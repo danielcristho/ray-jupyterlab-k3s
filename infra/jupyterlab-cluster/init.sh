9a9e7e039b3ff4769ac469964b8bd8e5325ff3c5
danielcristho <pepuhodaniel93.learn@gmail.com>
2025-01-16 06:14:06 +0700

    feat: update config

--- infra/jupyterlab-cluster/init.sh
+++ infra/jupyterlab-cluster/init.sh
@@ -2,5 +2,5 @@ helm upgrade --cleanup-on-fail \
   --install jhub jupyterhub/jupyterhub \
   --namespace jhub \
   --create-namespace \
-  --version=1.1 \
+  --version=4.0.0 \
   --values config.yaml
